b7cbf8bdc5a1ab9791937851a4b60604072b54f5
shaik sameer <shaiksameerforall@gmail.com>
2025-12-10 22:31:58 +0300

    add storage backend

--- app/page.tsx
+++ app/page.tsx
@@ -33,17 +33,15 @@ export default function App() {
     });
   }
 
- return (
+  return (
     <Authenticator>
       {({ signOut, user }) => (
         <main>
-          <h1>Hello {user?.username}</h1>
-          <button onClick={signOut}>Sign out</button>
-
+            <h1>Hello {user?.username}</h1>
+            <button onClick={signOut}>Sign out</button>
           {/* StorageBrowser Component */}
           <h2>Your Files</h2>
           <StorageBrowser />
-          
         </main>
       )}
     </Authenticator>
